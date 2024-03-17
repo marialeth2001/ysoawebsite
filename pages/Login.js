f32b7961b7af5cdaadb7c3c95ddea89947baee9f
marialeth2001 <162993072+marialeth2001@users.noreply.github.com>
2024-03-17 18:45:19 +0100

    Update Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -12,26 +12,29 @@ function Login() {
 	};
 
     	return (
-		<div className="login-container">
-			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="userType">I am a:</label>
-				<select id="userType" value={userType} onChange={(e) => setUserType(e.target.value)} required>
-					<option value="student">Student</option>
-					<option value="staff">Staff</option>
-					<option value="alumni">Alumni</option>
-				</select>
+		<div className="login-page"> 
+			<div className="login-container">
+				<h1>Login</h1>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="userType">I am a:</label>
+					<select id="userType" value={userType} onChange={(e) => setUserType(e.target.value)} required>
+						<option value="student">Student</option>
+						<option value="staff">Staff</option>
+						<option value="alumni">Alumni</option>
+					</select>
 
-				<label htmlFor="username">Username:</label>
-				<input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+					<label htmlFor="username">Username:</label>
+					<input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
 
-				<label htmlFor="password">Password:</label>
-				<input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+					<label htmlFor="password">Password:</label>
+					<input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
-				<button type="submit">Login</button>
-			</form>
+					<button type="submit">Login</button>
+				</form>
+			</div>
 		</div>
     );
 }
 
 export default Login;
+
