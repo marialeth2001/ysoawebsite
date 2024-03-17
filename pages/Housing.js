08022d79f39f28d4d4978490f10e718e1409d408
marialeth2001 <162993072+marialeth2001@users.noreply.github.com>
2024-03-17 18:44:56 +0100

    Update Housing.js

--- pages/Housing.js
+++ pages/Housing.js
@@ -6,30 +6,31 @@ class Housing extends React.Component {
 	render() 
 	{
 		return(
-			<main className="content-section-housing">
-				<h1 className="page-title-student">Student Housing</h1>
+			<div className="housing-page"> 
+				<main className="content-section-housing">
+					<h1 className="page-title-student">Student Housing</h1>
 
-				<section className="housing-options">
-					<article className="housing-item">
-						<img src={dorm} alt="Dorms"/>
-						<h3>Dormitories</h3>
-						<p>Traditional dorm living provides a vibrant community experience.</p>
-					</article>
-					<article className="housing-item">
-						<img src={apartments} alt="Apartments"/>
-						<h3>Apartments</h3>
-						<p>For those who need more space and privacy, we offer a range of apartments.</p>
-					</article>
-				</section>
-				<section className="housing-apply">
-					<article className="housing-item"> 
-						<h2>How to Apply</h2>
-						<p>Find out the process for securing your place in student housing.</p>
-						<a href="housing.html" class="apply-button">Apply for Housing</a>
-					</article>
-				</section>
-			</main>
-
+					<section className="housing-options">
+						<article className="housing-item">
+							<img src={dorm} alt="Dorms"/>
+							<h3>Dormitories</h3>
+							<p>Traditional dorm living provides a vibrant community experience.</p>
+						</article>
+						<article className="housing-item">
+							<img src={apartments} alt="Apartments"/>
+							<h3>Apartments</h3>
+							<p>For those who need more space and privacy, we offer a range of apartments.</p>
+						</article>
+					</section>
+					<section className="housing-apply">
+						<article className="housing-item"> 
+							<h2>How to Apply</h2>
+							<p>Find out the process for securing your place in student housing.</p>
+							<a href="housing.html" class="apply-button">Apply for Housing</a>
+						</article>
+					</section>
+				</main>
+			</div>
 		); 
 	}
 }
