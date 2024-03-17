f8c5b12080eb20d23b1b0020179a7d110f65ed62
marialeth2001 <162993072+marialeth2001@users.noreply.github.com>
2024-03-17 18:42:36 +0100

    Update App.js

--- App.js
+++ App.js
@@ -11,34 +11,43 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContainer from './components/MainContainer';
 
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
-import { BrowserRouter as Router, Routes, Route} from
-'react-router-dom';
 
 import './App.css';
 
 function App() {
-	return (			
-		<Router>						
-			<MainContainer> 
-				<Header />													
-					<Routes>
+	let location = useLocation();
 
-						<Route exact path="/" element={<Home />} />
-						<Route path="/artmajor" element={<Artmajor />} />
-						<Route path="/fact" element={<Fact />} />
-						<Route path="/visiting" element={<Visiting />} />
-						<Route path="/housing" element={<Housing />} />
-						<Route path="/login" element={<Login />} />
-								
-					</Routes>				
-				<Footer />					
-			</MainContainer>											
-		</Router>			
+	const isHousingOrLoginRoute = location.pathname === "/housing" || location.pathname === "/login";
+	const Content = isHousingOrLoginRoute ? React.Fragment : MainContainer;
+
+	return (
+		<Content>
+			<Header currentPath={location.pathname} />
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/artmajor" element={<Artmajor />} />
+				<Route path="/fact" element={<Fact />} />
+				<Route path="/visiting" element={<Visiting />} />
+				<Route path="/housing" element={<Housing />} />
+				<Route path="/login" element={<Login />} />
+		  	</Routes>
+			{!isHousingOrLoginRoute && <Footer />}
+			
+		</Content>
 	);
 }
 
-export default App;
+function AppWrapper() {
+	return (
+		<Router>
+			<App />
+	  	</Router>
+	);
+}
+      
+export default AppWrapper;
 
 
 
